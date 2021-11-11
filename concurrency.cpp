ab3fca6fafe93e143798ae261eb56ae67afb46f3
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-11-11 09:27:43 -0600

    created shared memory variable p

--- concurrency.cpp
+++ concurrency.cpp
@@ -3,6 +3,13 @@
 #include <time.h> //for time used to generate a seed
 #include <sys/shm.h>  //for shmget
 
+//*** From stackoverflow guide
+#include <sys/types.h> //key_t, sem_t, pid_t
+#include <stdlib.h>     //exit(), malloc(), free()
+#include <semaphore.h>   //sem_open, sem_destroy(), sem_wait() ..
+#include <fcntl.h>      //O_CREATE, O_EXEC
+
+//*********NOTE At LINE ~30 a specific directory is used************************
 void boardWriter(char board[][5]); //function to write board state
 char victory(char board[][5]); //function to determine if a player won
 void randomPlay(int& row, int& col, char board[][5], char player); //function to randomly select a location for play
@@ -16,19 +23,29 @@ int main()
   std::fill(gameboard[0], gameboard[0] + 5 * 5, '-');     //fill the empty spots with "-"
 
   //pid_t pid1, pid2, pid3;     //create ID's for three difference processes
+
+  key_t shmkey;     //shared memory key
+
+  //*********Directory Identified HERE************************
+  shmkey = ftok ("/dev/null", 5);       //valid directory name and number
+  //**************************************************************************
+  int shmid;    //shared memory id
+  shmid = shmget (shmkey, sizeof (int), 0644 |IPC_CREAT);
+  if (shmid < 0) {
+    perror("shmget\n");
+    exit (1);
+  }
+
+  int * p = (int * ) shmat (shmid, NULL, 0);   // attach p to shared memory
+  *p = 0;
+  printf ("p=%d is allocated in shared memory. \n\n", *p);
   boardWriter(gameboard);     //producer creates a blank gameboard
 
   //if (pid1=fork()==0)         //first child is created for Tic-Tac-Toe player "X"
 
 
-
-
-
-  int x, y;
-
-  std::cout<<"x is: " <<x <<" Y is: " <<y <<"\n";
-
 //**********TEST LOOP *************************
+  int x, y;
   for(int i{}; i<10; i++)
   {
     randomPlay(x,y, gameboard, 'X');
@@ -110,8 +127,7 @@ void randomPlay(int& row, int& col, char board[][5], char player)
     board[row][col] = player;
     boardWriter(board);
 }
-
-int sem_create()
+/*int sem_create()
 {
   if((shm_id = shmget(IPC_PRIVATE, sizeof(int), SHM_R | SHM_W)) < 0) {
     perror(NULL);
@@ -120,4 +136,5 @@ int sem_create()
   } else {
 
   }
-}
+  return  ;
+}*/
