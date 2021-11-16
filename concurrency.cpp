9a8e4f975a19909796bf2c0b75725be7a0be3f3e
j-knudson <71792574+j-knudson@users.noreply.github.com>
2021-11-15 19:01:32 -0600

    V1

--- concurrency.cpp
+++ concurrency.cpp
@@ -14,6 +14,7 @@
 #include <fcntl.h>          /* O_CREAT, O_EXEC          */
 
 #include <sys/wait.h>
+#include <sys/mman.h>
 
 //*********NOTE At LINE ~30 a specific directory is used************************
 void boardWriter(char board[][5]); //function to write board state
@@ -26,6 +27,9 @@ int main()
 {
   srand(time(NULL));      //generate a seed for the rand function
 
+
+
+  //char * gameboard[5][5];
   char gameboard[5][5];                                   //create a blank gameboard
   std::fill(gameboard[0], gameboard[0] + 5 * 5, '-');     //fill the empty spots with "-"
 
@@ -42,6 +46,14 @@ int main()
     perror("shmget\n");
     exit (1);
   }
+
+  //char gameboard[5][5];
+  //char* gameboard[5][5];
+  //gameboard = (char*) shmat(shmid, NULL, 0);
+  //gameboard = shmat(shmid, (void *)0, 0);
+  //gameboard = shmat(shmid, NULL, 0);
+  //gameboard[5][5];
+  std::fill(gameboard[0], gameboard[0] + 5 * 5, '-');
 
   int * p = (int * ) shmat (shmid, NULL, 0);   // attach p to shared memory
   *p = 0;
@@ -79,7 +91,7 @@ for(int i{}; i<25; i++)
 //**************</ TEST LOOP> ************
   return EXIT_SUCCESS;
 
-
+/*
 pid_t playerX, playerO, referee;     //create ID's for three difference processes
 
 
@@ -90,6 +102,7 @@ if (playerX < 0) {         //check for error
   std::cout <<"Fork Error \n";
   exit(0);
 }
+std::cout <<"playerX is: " << playerX;
 
 playerO = fork ();           //create fork for second player
 if (playerO < 0) {         //check for error
@@ -109,6 +122,8 @@ if (playerX < 0) {         //check for error
 
 pid_t pid;
 int i{};
+
+
 for (i; i < 3; i++) {
   pid = fork();
   if (pid < 0){
@@ -134,32 +149,103 @@ for (i; i < 3; i++) {
     std::cout <<"\n All children have exited. \n";
 
     //shared memory detach
-    shmdt (p);
+    shmdt (gameboard);
+    //shmdt (p);
     shmctl (shmid, IPC_RMID, 0);
 
-    /* cleanup semaphores */
+    // cleanup semaphores //
     sem_unlink ("producer");
     sem_close(sem);
-    /* unlink prevents the semaphore existing forever */
-    /* if a crash occurs during the execution         */
+    // unlink prevents the semaphore existing forever //
+    // if a crash occurs during the execution         //
     exit (0);
   }
 
   //********child
   else{
-      sem_wait (sem);           /* P operation */
+      sem_wait (sem);           // P operation //
       printf ("  Child(%d) is in critical section.\n", i);
       sleep (1);
-      *p += i % 3;              /* increment *p by 0, 1 or 2 based on i */
+      *p += i % 3;              //increment *p by 0, 1 or 2 based on i //
+      randomPlay(gameboard, i);
       printf ("  Child(%d) new value of *p=%d.\n", i, *p);
       //randomPlay(gameboard, i);
-      sem_post (sem);           /* V / Signal operation */
+      sem_post (sem);           // V / Signal operation //
       exit (0);
   }
+  */
+  ///******START OF TRIAL 2
+
+/*
+  int N=5; // Number of elements for the array
+
+  //int *ptr = mmap(NULL,N*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,0,0);
+
+  int shm_id;    //shared memory id
+  if((shm_id = shmget(IPC_PRIVATE, sizeof(int), SHM_R | SHM_W)) < 0){
+
+    perror(NULL);
+
+    printf("Can't Create Shared memory segment \n");
+
+  exit(1);
+
+  }
+  int rows {5}, columns{5};
+  char* name;
+  int shm_fd = shm_open(shm_id, O_CREAT | O_RDWR, 0666);
+  char gameboard[5][5];
+  char* ptr = mmap(0, 5*5, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  gameboard = (char(*)[columns])mmap(0, rows*columns,  PROT_WRITE,   MAP_SHARED,  shm_fd, 0);
+
+  if(ptr == MAP_FAILED){
+   printf("Mapping Failed\n");
+   return 1;
+  }
+
+
+    for(int i=0; i < N; i++){
+     ptr[i] = i + 1;
+    }
+
+    printf("Initial values of the array elements :\n");
+    for (int i = 0; i < N; i++ ){
+     printf(" %d", ptr[i] );
+    }
+    printf("\n");
+
+    pid_t child_pid = fork();
+
+    if ( child_pid == 0 ){
+     //child
+     for (int i = 0; i < N; i++){
+         ptr[i] = ptr[i] * 10;
+     }
+    }
+    else{
+     //parent
+     waitpid ( child_pid, NULL, 0);
+     printf("\nParent:\n");
+
+     printf("Updated values of the array elements :\n");
+     for (int i = 0; i < N; i++ ){
+         printf(" %d", ptr[i] );
+     }
+     printf("\n");
+    }
+
+    int err = munmap(ptr, N*sizeof(int));
+
+    if(err != 0){
+     printf("UnMapping Failed\n");
+     return 1;
+    }
+*/
+
   return EXIT_SUCCESS;
 }
 
-///*************FUNCTION DEFINITIONS*******************************
+//******************FUNCTION DEFINITIONS*******************************
 
 
 void boardWriter(char board[][5])
@@ -223,7 +309,7 @@ char victory(char board[][5])
               return winner = board[row][cols];}
       if(board[row][cols] == 'X' || board[row][cols] == 'O'){
         counter++;
-        std::cout<<"counter++ Space is: "<<board[row][cols]<< " counter is: "<< counter <<"\n";
+        //std::cout<<"counter++ Space is: "<<board[row][cols]<< " counter is: "<< counter <<"\n";
       }
       if(counter == 25)
         return winner = 'Z';  //magic character for tie condition
@@ -252,9 +338,9 @@ void randomPlay(char board[][5], char player)
 
     int random_max = 5;
     int row = rand() % random_max;
-    std::cout << "Row in randomPlay" << row <<"\n";
+    //std::cout << "Row in randomPlay" << row <<"\n";
     int col = rand() % random_max;
-    std::cout << "Col in randomPlay" << col <<"\n";
+    //std::cout << "Col in randomPlay" << col <<"\n";
     while(board[row][col] !='-')
     {
       row = rand() % random_max;
